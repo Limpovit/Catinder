87d77bf67f5598cdbdcff7ca6490be820670d0b5
Vitalii Lymar <vlm191997@gmail.com>
2020-05-14 14:14:17 +0300

    Fix custom view

--- Catinder/CardView.swift
+++ Catinder/CardView.swift
@@ -6,4 +6,30 @@
 //  Copyright © 2020 HexaHack. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class CardView: UIView {
+        
+    @IBOutlet var contentView: UIView!
+    @IBOutlet weak var emojiImageView: UIImageView!
+    @IBOutlet weak var catImageView: UIImageView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)        
+        commonInit()
+    }
+    
+    private func commonInit() {
+        Bundle.main.loadNibNamed("CardView" , owner: self, options: nil)
+        addSubview(contentView)        
+        contentView.frame = self.bounds
+        contentView.layer.cornerRadius = 20
+        contentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+    }
+    
+}
