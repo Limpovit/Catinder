6ec62d1bafa4db28ca81e843a94477e648b01c19
Vitalii Lymar <vlm191997@gmail.com>
2020-05-17 16:11:37 +0300

    Add and replace new images

--- Catinder/User/UserViewController.swift
+++ Catinder/User/UserViewController.swift
@@ -13,7 +13,7 @@ class UserViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        view.setGradient([ #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1).cgColor,  #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1).cgColor])
     }
     
 
