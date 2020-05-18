127155d2ffb9dad2fd556ea1a6fa875ffe0196bf
Vitalii Lymar <vlm191997@gmail.com>
2020-05-18 14:52:38 +0300

    Implement service locator

---
+++ Catinder/Services/ServiceLocator.swift
@@ -0,0 +1,43 @@
+//
+//  ServiceLocator.swift
+//  Catinder
+//
+//  Created by HexaHack on 17.05.2020.
+//  Copyright © 2020 HexaHack. All rights reserved.
+//
+
+import Foundation
+
+protocol  ServiceLocating {
+    func getService<T>() -> T?
+}
+
+final class ServiceLocator {
+    
+    
+    
+    private lazy var services: [String : Any] = [:]
+    
+    private func typeName(_ some: Any) -> String {
+        return (some is  Any.Type) ? "\(some)" : "\(type(of: some))"
+    }
+    
+    private func addService<T>(service: T) {
+        let key = typeName(T.self)
+        services[key] = service
+    }
+    func getService<T>() -> T? {
+        let key = typeName(T.self)
+        return services[key] as? T
+    }
+    public static let shared = ServiceLocator()
+    
+    private init() {
+        let apiService = APIService()
+        let imageService = ImagesService(apiService: apiService)
+        addService(service: apiService as APIServiceProtocol)
+        addService(service: imageService as ImagesServiceProtocol)
+    }
+    
+}
+
