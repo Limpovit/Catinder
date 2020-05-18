d12d49f5c04c7a1009cfa3151b2771200a1d8544
Vitalii Lymar <vlm191997@gmail.com>
2020-05-18 14:59:52 +0300

    Mock up user view in storyboard

--- Catinder/User/UserViewController.swift
+++ Catinder/User/UserViewController.swift
@@ -16,15 +16,6 @@ class UserViewController: UIViewController {
         view.setGradient([ #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1).cgColor,  #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1).cgColor])
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
